4249e4f4530f81f6b1ec84640db3ff37a4a73d9c
blandhalen <blandhalen@blands-imac.home>
2018-07-06 10:46:13 -0400

    express running

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('tofu baby'); 
+const express = require('express'); 
+const app = express(); 
+
+app.get('/', (req, res)=>{
+    res.send('tofu baby baby'); 
+}); 
+
+app.listen(3000, ()=>{
+    console.log('listening....'); 
+}); 
