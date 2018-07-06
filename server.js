b835c7079d58f5783d24562b5db58b678b517bef
blandhalen <blandhalen@blands-imac.home>
2018-07-06 16:20:10 -0400

    create route complete

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
+//EXPRESS
 const express = require('express'); 
 const app = express(); 
+//RETURN HEROKU OR LOCAL 3000
 const PORT = process.env.PORT || 3000;
-
+// MONGOOSE DATABASE
 const mongoose = require('mongoose'); 
+//HEROKU PORT
 const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
 
-app.get('/', (req, res)=>{
-    res.send('tofu baby baby'); 
+// ROUTES
+
+//CREATE ROUTE
+app.get('/coin/new', (req, res)=>{
+    res.render('new.ejs'); 
 }); 
 
+// LISTENER
 app.listen(PORT, ()=>{
     console.log('listening....'); 
 }); 
 
+// CONNECT TO MONGO
 mongoose.connect(mongoUri, { useNewUrlParser: true });
 mongoose.connection.on('open', ()=> {
     console.log('connected to mongoose!!')
