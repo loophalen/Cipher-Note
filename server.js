ae8ba15ab4292ec1dbeb21accbcd31db9951dde4
blandhalen <blandhalen@blands-imac.home>
2018-07-07 18:14:38 -0400

    delete route complete

--- server.js
+++ server.js
@@ -10,10 +10,63 @@ const Coin = require('./models/coins.js');
 //HEROKU PORT
 const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/coin_app_dev';
 
+//METHOD-OVERRIDE
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
 //MIDDLEWARE
 app.use(express.urlencoded({extended:true}));
 
 // ROUTES
+
+//SEED ROUTE
+app.get('/coin/seed', (req, res)=>{
+    Coin.create([
+        {
+            "coin": "Bitcoin",  
+            "symbol": "BTC"  
+        },
+        {
+            "coin": "Ethereum", 
+            "symbol": "ETH" 
+        },
+        {
+            "coin": "Litecoin",
+            "symbol": "LTC" 
+        },
+        {
+            "coin": "Ripple",
+            "symbol": "XRP"
+        }, 
+        {
+            "coin": "Bitcoin Cash",  
+            "symbol": "BCH" 
+        },
+        {
+            "coin": "Ethereum Classic",
+            "symbol": "ETC"
+        },
+        {
+            "coin": "Monero", 
+            "symbol": "XMR" 
+        },
+        {
+            "coin": "Stellar", 
+            "symbol": "XLM"
+        },
+        {
+            "coin": "Dash",
+            "symbol": "DASH"
+        },
+        {
+            "coin": "Decred", 
+            "symbol": "DCR"
+        },
+
+    ], (err, data)=>{
+        res.redirect('/coin');
+    })
+}); 
 
 //CREATE NEW ROUTE
 app.get('/coin/new', (req, res)=>{
@@ -45,10 +98,36 @@ app.get('/coin', (req, res)=>{
 app.get('/coin/:id', (req, res)=>{
     Coin.findById(req.params.id, (err, foundCoin)=>{
         res.render('show.ejs', {
-            coin:Coin
+            coin:foundCoin
         });
     });
 });
+
+//TESTING API ///
+// app.get('/coin/:id', (req, res) => {
+//     Coin.findById(req.someId, (err, foundCoin) => {
+//       const url = `https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=${foundCoin.coinId}`;
+//       axios.get(url)
+//         .then((coinData) => {
+//           console.log(coinData);
+//           res.render('show.ejs', {
+//             coin: foundCoin,
+//             coindData: coinData,
+//           });
+//         }
+//         );
+//     });
+//   });
+
+//DELETE ROUTE
+app.delete('/coin/:id', (req, res)=>{
+    Coin.findByIdAndRemove(req.params.id, (err, data)=>{
+        res.redirect('/coin'); 
+    }); 
+}); 
+
+
+
 
 
 // LISTENER
